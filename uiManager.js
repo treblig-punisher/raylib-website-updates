755fa35f9abc940f98a479be9aac368e84e864e4
treblig-punisher <gilbertplayx@hotmail.com>
2024-05-07 01:44:45 -0400

    no true changes other than code testing. The go to top button doesn't go away when entering print mode.

--- uiManager.js
+++ uiManager.js
@@ -1,12 +1,12 @@
-const backToTopArrow = document.querySelector(".back-to-top-arrow");
-window.addEventListener("scroll", (e) =>
+const backToTopButton = document.querySelector(".back-to-top-arrow");
+window.addEventListener("scroll", () =>
 {
     if (window.scrollY <= 200){
-         backToTopArrow.style.display = "none"; 
-         backToTopArrow.classList.remove("spring-anim-class");
+         backToTopButton.style.display = "none"; 
+         backToTopButton.classList.remove("spring-anim-class");
     }
     else {
-        backToTopArrow.style.display = "flex";
-        backToTopArrow.classList.add("spring-anim-class");
+        backToTopButton.style.display = "flex";
+        backToTopButton.classList.add("spring-anim-class");
     }
 });
